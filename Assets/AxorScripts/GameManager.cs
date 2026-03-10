e2f704118a98c93e87ed9dc3d2e05863281bcbab
Mia Trujillo <mia.trujillo@uniat.edu.mx>
2026-03-10 12:29:23 -0600

    Player Interaction with Collisions
    - Made GameManager a Singleton
    - Made colliders work alongisde game over
    - Player can now step on the estepicursor.

--- Assets/AxorScripts/GameManager.cs
+++ Assets/AxorScripts/GameManager.cs
@@ -2,11 +2,24 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
     public static bool gameOver = false;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
 
     public void GameOver()
     {
         gameOver = true;
         GameSpeed.speed = 0;
+        Debug.Log("Game Over");
     }
 }
---
+++ Assets/Scripts/Obstacles/CollidingWithObstacle.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CollidingWithObstacle : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GameManager.instance.GameOver();
+            //Debug.Log("Player collided with: " + gameObject.name + " and lost.");
+        }
+    }
+}
