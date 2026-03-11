11294648641c93e66124b5f3d087bd8c5525927e
Angel Tapia <angel.sanchez@uniat.edu.mx>
2026-03-10 18:38:49 -0600

    Revert "Playable Demo V1.1"
    This reverts commit dd5e5b577c17b63e86192a07b570dc52ea80d348.

--- Assets/AxorScripts/GameManager.cs
+++ Assets/AxorScripts/GameManager.cs
@@ -2,39 +2,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance;
+    public static bool gameOver = false;
 
-    public float speed = 6f;
-    public float maxSpeed = 18f;
-    public float acceleration = 0.02f;
-
-    public float distanceTravelled;
-    public bool gameOver;
-
-    void Awake()
+    public void GameOver()
     {
-        instance = this;
-    }
-
-    void Update()
-    {
-        if (!gameOver)
-        {
-            if (speed < maxSpeed)
-                speed += acceleration * Time.deltaTime;
-
-            distanceTravelled += speed * Time.deltaTime;
-        }
-    }
-
-    public int GetDifficulty()
-    {
-        if (distanceTravelled < 200)
-            return 0;
-
-        if (distanceTravelled < 500)
-            return 1;
-
-        return 2;
+        gameOver = true;
+        GameSpeed.speed = 0;
     }
 }
--- Assets/AxorScripts/MoveLeft.cs
+++ Assets/AxorScripts/MoveLeft.cs
@@ -2,19 +2,11 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
-    public float disableX = -15f;
-    public bool returnToPool = false;
-
     void Update()
     {
-        if (GameManager.instance.gameOver)
-            return;
-
-        transform.position += Vector3.left * GameManager.instance.speed * Time.deltaTime;
-
-        if (returnToPool && transform.position.x < disableX)
+        if (!GameManager.gameOver)
         {
-            gameObject.SetActive(false);
+            transform.position += Vector3.left * GameSpeed.speed * Time.deltaTime;
         }
     }
 }
